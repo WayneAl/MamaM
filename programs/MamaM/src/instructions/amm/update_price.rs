af3d4cdf867de47820c6cbd02000f25efdda2b21
Wayne Kuo <wayne.k@alphabit.software>
2022-04-23 15:23:02 +0800

    Update update_price.rs

--- programs/MamaM/src/instructions/amm/update_price.rs
+++ programs/MamaM/src/instructions/amm/update_price.rs
@@ -1,12 +1,14 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
-use pyth_client::Price;
 
 #[derive(Accounts)]
 pub struct UpdatePrice<'info> {
-    pub oracle: Account<'info, Price>, //TODO: get oracle data from Pyth
+    pub oracle: AccountInfo<'info>, //TODO: get oracle data from Pyth
 }
 
 pub fn handle(ctx: Context<UpdatePrice>) -> ProgramResult {
+    let oracle_data = ctx.accounts.oracle.try_borrow_data()?;
+    let price_account = pyth_client::load_price(&oracle_data).unwrap();
+
     Ok(())
 }
