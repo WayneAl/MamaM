4b0c4a06f8ad2b96fe42eff7c9fc5c65db042cd7
Wayne Kuo <wayne.k@alphabit.software>
2022-04-23 16:13:06 +0800

    Move Ema to AMM

--- programs/MamaM/src/instructions/state/amm.rs
+++ programs/MamaM/src/instructions/state/amm.rs
@@ -7,5 +7,29 @@ use super::MarketData;
 pub struct Amm {
     pub market: MarketData,
 
-    pub ema: u64,
+    pub ema: Ema,
+}
+
+#[derive(Clone, Debug, PartialEq, AnchorSerialize, AnchorDeserialize)]
+pub struct Ema {
+    pub value: u64,
+    pub length: u64,
+    pub time_granularity: u64, // in secs
+    pub timestamp: u64,        // in secs
+}
+
+impl Ema {
+    /// init Ema
+    pub fn new(&mut self, length: u64, time_granularity: u64) {
+        self.length = length;
+        self.time_granularity = time_granularity;
+        self.timestamp = Clock::get().unwrap().unix_timestamp as u64;
+
+        self.value = 0; // TODO
+    }
+
+    /// update Ema
+    pub fn next(price: f64) {
+        // calculation
+    }
 }
--- programs/MamaM/src/instructions/state/mod.rs
+++ programs/MamaM/src/instructions/state/mod.rs
@@ -1,5 +1,4 @@
 pub mod amm;
-pub mod ema;
 pub mod exchange;
 
 pub use exchange::*;
